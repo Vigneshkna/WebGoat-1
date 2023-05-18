4929b6178750e5d8f2a48c2a9e3194ca6e629af8
VigneshKanna <35601923+Vigneshkna@users.noreply.github.com>
2023-05-18 15:20:39 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
     githubToken1: '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',
-    githubToken11: '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',
+    githubToken11: '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',
     githubToken2: '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'
   };
   
